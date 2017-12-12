ad84bda55491c9ccbeb07b23679b6264f0820b5c
bambuk77 <34056061+bambuk77@users.noreply.github.com>
2017-12-12 13:31:13 +0300

    1.2

--- scala/Choice.java
+++ scala/Choice.java
@@ -1,19 +1,64 @@
 import java.util.Scanner;
-import class.Calc;
-import class.Array;
 public class Choice {
     public static void main(String[] args) {
+        System.out.println("Enter number 1 if you want to use calc or number 2 if you need to find the longest word");
         Scanner scanner = new Scanner(System.in);
-        char a = scanner.next().charAt(0);
-        if (a==1){
+        int q = scanner.nextInt();
+        if (q == 1)
+        {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter first number");
+            double i = sc.nextFloat();
+            System.out.println("Enter mathematic action");
+            char mult = sc.next().charAt(0);
+            System.out.println("Enter second number");
+            double y = sc.nextFloat();
+            sc.close();
 
-            Calc cal = new Calc();
-            
+            switch (mult)
+            {
+                case '+':
+                    System.out.print("Addition: ");
+                    System.out.printf("%.4f", i + y);
+                    break;
+                case '-':
+                    System.out.print("Subtraction: ");
+                    System.out.printf("%.4f", i - y);
+                    break;
+                case '*':
+                    System.out.print("Multiplication: ");
+                    System.out.printf("%.4f", i * y);
+                    break;
+                case '/':
+                    System.out.print("Division: ");
+                    System.out.printf("%.4f", i / y);
+                    break;
+                default:
+                    System.out.print("Error+");
+            }
         }
-
         else{
-            Array array = new Array();
-        }
+            System.out.println("Enter amount of words");
+            Scanner sca = new Scanner(System.in);
+            int i = sca.nextInt();
+            String[] words;
+            words = new String[i];
+            int j = 0;
+            String a = (" ");
+            int b = 0;
+            System.out.println("Enter "+i+" words");
+            while(j < i){
+                Scanner scan = new Scanner(System.in);
+                words[j] = scan.nextLine();
 
+                if (words[j].length() >= a.length()) {
+                    a = words[j];
+                    b = j;
+                }
+                j++;
+            }
+            System.out.println("The longest word is "+"'"+words[b]+"'");
+        }
+        }
     }
-}
+
